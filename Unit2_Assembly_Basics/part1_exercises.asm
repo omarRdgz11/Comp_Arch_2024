5549deb9268e853c198a2ae246b241bbef39feb7
Omar A. Carmona <omar.rdgz04@gmail.com>
2024-01-30 21:14:24 +0000

    initial part1_exercises

--- Unit2_Assembly_Basics/part1_exercises.asm
+++ Unit2_Assembly_Basics/part1_exercises.asm
@@ -23,7 +23,64 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+		; dump_regs 1	; TODO: add code
+        ; call print_nl
+        ; dump_regs 2
+
+        ; mov eax, 0
+        ; mov ah, 18
+        ; mov al, 0BAH
+        ; dump_regs 1
+        ; call print_nl
+
+        ; mov ax, 4
+        ; dump_regs 2
+
+        ; mov eax, 3
+        ; mov ebx, 4
+        ; add eax, ebx
+        ; dump_regs 1
+
+        ; mov eax, 34
+        ; sub eax, 19
+        ; dump_regs 1
+
+        ; mov eax, -32
+        ; dump_regs 2
+
+        ; mov eax, 16
+        ; dump_regs 1
+        ; call print_nl
+
+        ; sub eax, 4
+        ; dump_regs 2
+        ; call print_nl
+
+        ; mov ebx, eax
+        ; dump_regs 3
+        ; call print_nl
+
+        ; inc ebx
+        ; dump_regs 4
+
+        mov eax, 4
+        mov ebx, 4
+        mov ecx, 4
+
+        add eax, ebx
+        add ebx, ecx
+        add ecx, eax
+
+        mov ebx, 4
+
+        add ebx, eax
+        add ebx, ebx
+        mov ecx, ebx
+
+        add ebx, ebx
+
+        add ebx, ecx
+        dump_regs 1
 
         popa
         mov     eax, 0            ; return back to C
